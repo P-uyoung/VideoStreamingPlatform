7494a27dfa4fe859500cf358d51018a4622d54cd
P-uyoung <wooyoung9654@gmail.com>
2024-02-12 23:21:16 +0900

    Add Header Component
    using angular material (like bootstrap)

--- frontend/youtube-clone-ui/src/app/app.module.ts
+++ frontend/youtube-clone-ui/src/app/app.module.ts
@@ -7,12 +7,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxFileDropModule } from 'ngx-file-drop';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './header/header.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
 declarations: [
   AppComponent,
   UploadVideoComponent,
+  HeaderComponent,
 ],
 imports: [
   BrowserModule,
@@ -20,6 +25,9 @@ imports: [
   FormsModule,
   HttpClientModule,
   NgxFileDropModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule
 ],
   providers: [
     provideClientHydration(),
