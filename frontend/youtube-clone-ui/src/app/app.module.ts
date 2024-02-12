7518967873b9779c5a0266b58fe16439129deb54
P-uyoung <wooyoung9654@gmail.com>
2024-02-12 22:49:22 +0900

    Angular - Upload Videos To AWS S3

--- frontend/youtube-clone-ui/src/app/app.module.ts
+++ frontend/youtube-clone-ui/src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadVideoComponent } from './upload-video/upload-video.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxFileDropModule } from 'ngx-file-drop';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+declarations: [
+  AppComponent,
+  UploadVideoComponent,
+],
+imports: [
+  BrowserModule,
+  AppRoutingModule,
+  FormsModule,
+  HttpClientModule,
+  NgxFileDropModule,
+],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
   ],
   bootstrap: [AppComponent]
 })
